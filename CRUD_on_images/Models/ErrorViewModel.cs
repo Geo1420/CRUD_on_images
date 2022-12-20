60d4233513a9d42b34b24e79fdbb91523bcab24c
marina.cristian.k2y <marina.cristian.k2y@student.ucv.ro>
2022-12-20 20:17:56 +0200

    Test

--- CRUD_on_images/Models/ErrorViewModel.cs
+++ CRUD_on_images/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace CRUD_on_images.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? RequestId  { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
